31dc1d32d4c40a9ae26a6fc61f416e20362ebef3
W-B28 <50033779+W-B28@users.noreply.github.com>
2019-08-25 20:22:30 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+/* click event on submit button that captures fields
+ click event on clear button that clears fields
+submit button clear()
+
+take info and place into an array of objects
+ */
+
+ var clear = document.getElementById("myClear").addEventListener("click", function() {
+   alert("CLEAR!");
+ });
+
+ var submit = document.getElementById("mySubmit").addEventListener("click", function() {
+   alert("SUBMIT!");
+ });
