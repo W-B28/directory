fd12d07ae00e123ef5840d6ec7428311aa039be5
W-B28 <50033779+W-B28@users.noreply.github.com>
2019-08-27 17:59:09 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,50 @@
 /* click event on submit button that captures fields
  click event on clear button that clears fields
-submit button clear()
+submit button captures user entered values
+take all the values we collected, store them in a user object inside that click addEventListener
 
-take info and place into an array of objects
  */
 
- var clear = document.getElementById("myClear").addEventListener("click", function() {
-   alert("CLEAR!");
+let directory = [];
+
+// Function that loops through the directory and appends each element to the document
+//    for loop
+//         append each element preformatted with jquery
+function renderToDom() {
+  $('.results').empty();
+
+  for (let x = 0; x < directory.length; x++) {
+    $('.results').append('<div class="card"><h3>' + directory[x].fullName + '</h3></div>');
+  }
+}
+
+// BUTTONS
+document.getElementById("myClear").addEventListener("click", function () {
+   $("#firstName").remove();
+   $(".first-name-group").append('<input type="text" class="form-control" id="firstName" placeholder="Enter Here">');
+   $("#lastName").remove();
+   $(".last-name-group").append('<input type="text" class="form-control" id="lastName" placeholder="Enter Here">');
+   $("#eMail").remove();
+   $(".eMail-group").append('<input type="text" class="form-control" id="eMail" placeholder="name@email.com">');
+   $("#phoneNumber").remove();
+   $(".phone-group").append('<input type="text" class="form-control" id="phoneNumber" placeholder="123-867-5309">');
  });
 
- var submit = document.getElementById("mySubmit").addEventListener("click", function() {
-   alert("SUBMIT!");
+document.getElementById("mySubmit").addEventListener("click", function () {
+   let userInputtedFirstName = $("#firstName").val();
+   let userInputtedLastName = $("#lastName").val();
+   let userInputtedeMail = $("#eMail").val();
+   let userInputtedPhoneNumber = $("#phoneNumber").val();
+
+    let tempObj = {};
+    tempObj.fullName = userInputtedFirstName + " " + userInputtedLastName;
+    tempObj.email = userInputtedeMail;
+    tempObj.phone = userInputtedPhoneNumber;
+
+    directory.push(tempObj);
+
+    // SET ALL FIELDS = to original .val()
+    renderToDom();
+
+
  });
